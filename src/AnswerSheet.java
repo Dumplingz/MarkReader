4801940b776925f6330168b58df32352500c4a90
efraisse694 <efraisse694@fhs.win.fuhsd.org>
2017-03-01 10:44:42 -0800

    work on processImage

--- src/AnswerSheet.java
+++ src/AnswerSheet.java
@@ -1,6 +1,23 @@
+import processing.core.PImage;
+
 /***
  * A class to represent a set of answers from a page
  */
 public class AnswerSheet {
+	private PImage image;
+	private String[] answers;
+
+	public AnswerSheet(PImage image, String[] answers) {
+		this.image = image;
+		this.answers = answers;
+	}
+
+	public PImage getImage() {
+		return image;
+	}
+
+	public String[] getAnswers() {
+		return answers;
+	}
 
 }
--- src/OpticalMarkReader.java
+++ src/OpticalMarkReader.java
@@ -5,14 +5,46 @@ import processing.core.PImage;
  * 
  */
 public class OpticalMarkReader {
-
+	// find where the numbers are using polychrome filter strategy
 	/***
-	 * Method to do optical mark reading on page image.  Return an AnswerSheet object representing the page answers.
+	 * Method to do optical mark reading on page image. Return an AnswerSheet
+	 * object representing the page answers.
+	 * 
 	 * @param image
 	 * @return
 	 */
 	public AnswerSheet processPageImage(PImage image) {
-		
+		processImage(image);
+		for (int row = 0; row < image.height; row++) {
+			for (int col = 0; col < image.width; col++) {
+
+			}
+		}
 		return null;
+	}
+
+	public void processImage(PImage image) {
+		for (int row = 0; row < image.height; row++) {
+			for (int col = 0; col < image.width; col++) {
+				int color = getPixelAt(row, col, image);
+				if (color > 255) {
+					System.out.println("row: " + row + " col: " + col + " color: " + color);
+				}
+				if (color < 123) {
+					image.set(row, col, 0);
+				} else {
+					image.set(row, col, 255);
+				}
+			}
+		}
+
+	}
+
+	public int getPixelAt(int row, int col, PImage image) {
+		image.loadPixels();
+
+		int index = row * image.width + col;
+
+		return image.pixels[index];
 	}
 }
