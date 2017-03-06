3c67e671927132b4fbd14c16977a60e8e31bd458
czhu906 <czhu906@fhs.win.fuhsd.org>
2017-03-06 11:45:11 -0800

    fix processImage for future image processing

--- src/OpticalMarkReader.java
+++ src/OpticalMarkReader.java
@@ -1,3 +1,4 @@
+import processing.core.PApplet;
 import processing.core.PImage;
 
 /***
@@ -12,42 +13,60 @@ public class OpticalMarkReader {
 	 * @param image
 	 * @return
 	 */
-	public AnswerSheet processPageImage(PImage image) {
-		processImage(image);
+	public AnswerSheet processPageImage(PImage image, PApplet window) {
+		processImage(image, window);
 		AnswerSheet sheet = new AnswerSheet(image);
 		for (int row = 110; row < image.height; row++) {
 			for (int col = 0; col < image.width; col++) {
-				
+
 			}
 		}
 		return null;
 	}
 
-	public static void processImage(PImage image) {
+	public static void processImage(PImage image, PApplet window) {
 		image.filter(PImage.GRAY);
 		for (int row = 0; row < image.height; row++) {
 			for (int col = 0; col < image.width; col++) {
-				int color = getPixelAt(row, col, image);
+				int color = image.get(col, row);
+				float grayval = (float) (window.red(color));
 
-				  // System.out.println("row: " + row + " col: " + col + " color: " + color);
+				// System.out.println(grayval);
+				image.updatePixels();
+				color = 0;
 
-			
-					image.set(row, col, 255);
-			
-					//image.set(row, col, 255);
-				
+				int newVal = 100;
+
+				image.set(col, row, window.color(newVal, newVal, newVal));
+
+				// setPixelAt(row, col, color, image);
+
+				// System.out.println("row: " + row + " col: " + col + " color:
+				// " + color);
+				//
+				// if (color > 123)
+				// image.set(row, col, 2);
+				// else
+				// image.set(row, col, 0);
+				// image.set(row, col, 0);
 			}
 		}
-		
-		image.updatePixels();
 
+	}
+
+	private static void setPixelAt(int row, int col, int color, PImage image) {
+		image.loadPixels();
+
+		int index = row * image.width + col;
+
+		image.pixels[index] = (color << 16) + (color << 8) + color;
 	}
 
 	public static int getPixelAt(int row, int col, PImage image) {
 		image.loadPixels();
 
 		int index = row * image.width + col;
-	
+
 		return image.pixels[index] & 255;
 	}
 
